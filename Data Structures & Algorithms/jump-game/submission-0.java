5700b7a8506eed4fb21580b52771a34dd94ed989
sapanthichowdary <147791772+sapanthichowdary@users.noreply.github.com>
2026-09-07 14:22:49 +0530

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int max = 0;
+        int n = nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(i>max) return false;
+            max = Math.max(max,i+nums[i]);
+            if(n-1<=max)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
